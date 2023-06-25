5d4dad5996232fbb413306a2403c6c0f151e0c56
starydv7 <pawanstar72926@gmail.com>
2023-06-25 23:20:14 +0530

    backend

--- client/src/pages/Policy.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/Layout/Layout";
-
-const Policy = () => {
-  return <Layout title={"Policy-Gammavit-ShopNow"}>Policy</Layout>;
-};
-
-export default Policy;
